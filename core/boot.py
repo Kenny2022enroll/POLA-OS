522a1b1923d3a91b9e5df3ce82904922ea13710f
Kenny <121385153+Kenny2022enroll@users.noreply.github.com>
2026-07-26 14:37:57 +0800

    更新 boot.py

--- core/boot.py
+++ core/boot.py
@@ -1,23 +1,28 @@
+from drivers.display import Display
+from core.scheduler import Scheduler
 from core.app_manager import AppManager
-
 from apps.timer import Timer
 from apps.settings import Settings
+from ui.launcher import Launcher
 
 class Boot:
     def __init__(self):
-        self.display=Display()
-        self.scheduler=Scheduler(
+        self.display = Display()
+        self.scheduler = Scheduler(
             fps=10
         )
-        self.app_manager=AppManager()
+        self.app_manager = AppManager()
         self.app_manager.register(
             Timer()
         )
         self.app_manager.register(
             Settings()
         )
-        self.launcher=Launcher(
+        self.launcher = Launcher(
             self.display,
             self.scheduler,
             self.app_manager
         )
+
+    def start(self):
+        self.launcher.run()
