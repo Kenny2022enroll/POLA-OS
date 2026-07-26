60ecf2ed49b845675e8e7e59f7be642b86eab01b
Kenny <121385153+Kenny2022enroll@users.noreply.github.com>
2026-07-26 14:00:40 +0800

    更新 boot.py

--- core/boot.py
+++ core/boot.py
@@ -1,11 +1,16 @@
 from drivers.display import Display
 from ui.launcher import Launcher
+from core.scheduler import Scheduler
 
 class Boot:
     def __init__(self):
         self.display = Display()
+        self.scheduler = Scheduler(
+            fps=10
+        )
         self.launcher = Launcher(
-            self.display
+            self.display,
+            self.scheduler
         )
 
     def start(self):
