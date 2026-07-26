c8c53ac8569e59afdbee5068acef2cf278299122
Kenny <121385153+Kenny2022enroll@users.noreply.github.com>
2026-07-26 14:04:47 +0800

    更新 timer.py

--- apps/timer.py
+++ apps/timer.py
@@ -2,23 +2,29 @@ from core.app import App
 import time
 
 class Timer(App):
+
     name="Timer"
+
     def __init__(self):
         self.start=time.time()
+        self.value=0
 
-    def draw(self,display):
-        t=int(
-            time.time()
-            -
-            self.start
+    def update(self):
+        self.value=int(
+            time.time()-self.start
         )
+
+    def draw(
+        self,
+        display
+    ):
         display.text(
             "Timer",
             40,
-            10
+            25
         )
         display.text(
-            str(t),
+            str(self.value),
             55,
-            30
+            40
         )
