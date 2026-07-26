b1bc3a2bc7975b13d37ad391c50a7493dc98dc5f
Kenny <121385153+Kenny2022enroll@users.noreply.github.com>
2026-07-26 14:04:00 +0800

    更新 app.py

--- core/app.py
+++ core/app.py
@@ -1,13 +1,23 @@
 class App:
     name="APP"
+
     def open(self):
         pass
 
     def update(self):
         pass
 
-    def draw(self,display):
+    def on_event(
+        self,
+        event
+    ):
         pass
 
+    def draw(
+        self,
+        display
+    ):
+        pass
+        
     def close(self):
         pass
