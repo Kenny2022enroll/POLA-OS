7f10aa0015cae680625b919299cf27a708696363
Kenny <121385153+Kenny2022enroll@users.noreply.github.com>
2026-07-27 12:58:38 +0800

    更新 event.py

--- core/event.py
+++ core/event.py
@@ -1,21 +1,21 @@
 class Event:
-    def __init__(
-        self,
-        name
-    ):
-        self.name=name
+    def __init__(self, type):
+        self.type = type
+
+# 系统事件类型
+BUTTON_A = "button_a"
+BUTTON_B = "button_b"
+UP = "up"
+DOWN = "down"
 
 class EventManager:
     def __init__(self):
-        self.queue=[]
-        
-    def emit(
-        self,
-        event
-    ):
+        self.queue = []
+
+    def emit(self,event):
         self.queue.append(event)
 
-    def get(self):
-        if self.queue:
+    def poll(self):
+        if len(self.queue)>0:
             return self.queue.pop(0)
         return None
