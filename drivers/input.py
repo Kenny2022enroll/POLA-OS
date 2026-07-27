d65401f50f7d527d617ee59f2719d1eef5832bba
Kenny <121385153+Kenny2022enroll@users.noreply.github.com>
2026-07-27 12:59:27 +0800

    更新 input.py

--- drivers/input.py
+++ drivers/input.py
@@ -1,12 +1,20 @@
+from mpython import *
 from core.event import Event
+from core.event import BUTTON_A, BUTTON_B
 
 class Input:
     def __init__(
         self,
         event_manager
     ):
-        self.events=event_manager
+        self.events = event_manager
 
     def update(self):
-        # 这里以后接真实按键
-        pass
+        if button_a.value()==0:
+            self.events.emit(
+                Event(BUTTON_A)
+            )
+        if button_b.value()==0:
+            self.events.emit(
+                Event(BUTTON_B)
+            )
