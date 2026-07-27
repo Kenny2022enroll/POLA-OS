a44eb2e5e6b583dd829e661800b0ca5a0b3bc27d
Kenny <121385153+Kenny2022enroll@users.noreply.github.com>
2026-07-27 13:37:05 +0800

    更新 event.py

--- core/event.py
+++ core/event.py
@@ -1,9 +1,15 @@
 class Event:
-    def __init__(self,type):
+    def __init__(self, type):
         self.type = type
-
+# 按键事件
 BUTTON_A = "button_a"
 BUTTON_B = "button_b"
+
+# 方向事件
+BUTTON_UP = "button_up"
+BUTTON_DOWN = "button_down"
+BUTTON_LEFT = "button_left"
+BUTTON_RIGHT = "button_right"
 
 class EventManager:
     def __init__(self):
