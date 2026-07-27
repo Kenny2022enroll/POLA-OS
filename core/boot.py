bfcc30f13edeb6b3b062ffda1e5d548710da1b9a
Kenny <121385153+Kenny2022enroll@users.noreply.github.com>
2026-07-27 14:13:46 +0800

    更新 boot.py

--- core/boot.py
+++ core/boot.py
@@ -1,30 +1,36 @@
+from drivers.display import Display
+from drivers.input import Input
+from core.scheduler import Scheduler
+from core.event import EventManager
+from core.app_manager import AppManager
 from core.navigation import Navigation
 from core.kernel import Kernel
+from apps.registry import load_apps
 from ui.launcher import Launcher
 
 class Boot:
     def __init__(self):
-        self.display=Display()
-        self.scheduler=Scheduler(
+        self.display = Display()
+        self.scheduler = Scheduler(
             fps=10
         )
-        self.events=EventManager()
-        self.input=Input(
+        self.events = EventManager()
+        self.input = Input(
             self.events
         )
-        self.app_manager=AppManager()
+        self.app_manager = AppManager()
         self.app_manager.load(
             load_apps()
         )
-        self.navigation=Navigation()
-        launcher=Launcher(
+        self.navigation = Navigation()
+        launcher = Launcher(
             self.app_manager,
             self.navigation
         )
         self.navigation.push(
             launcher
         )
-        self.kernel=Kernel(
+        self.kernel = Kernel(
             self.display,
             self.scheduler,
             self.input,
