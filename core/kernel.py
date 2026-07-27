8c603856e737cf151b2ea5da8344488aef1de639
Kenny <121385153+Kenny2022enroll@users.noreply.github.com>
2026-07-27 13:21:42 +0800

    创建 kernel.py

---
+++ core/kernel.py
@@ -0,0 +1,38 @@
+class Kernel:
+    def __init__(
+        self,
+        display,
+        scheduler,
+        input,
+        events,
+        navigation
+    ):
+        self.display=display
+        self.scheduler=scheduler
+        self.input=input
+        self.events=events
+        self.navigation=navigation
+
+    def run(self):
+        while True:
+            self.scheduler.wait()
+            # 输入
+            self.input.update()
+            # 事件
+            event=self.events.poll()
+            if event:
+                page=self.navigation.current()
+                if page:
+                    result=page.on_event(
+                        event
+                    )
+                    if result=="BACK":
+                        self.navigation.pop()
+            page=self.navigation.current()
+            if page:
+                page.update()
+                self.display.clear()
+                page.draw(
+                    self.display
+                )
+                self.display.update()
