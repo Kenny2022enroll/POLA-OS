807c4ffaf48a50ddccaf31ec8fecbc8b128712fa
Kenny <121385153+Kenny2022enroll@users.noreply.github.com>
2026-07-27 13:23:26 +0800

    创建 widget.py

---
+++ ui/widget.py
@@ -0,0 +1,18 @@
+class Widget:
+    def __init__(
+        self,
+        x=0,
+        y=0,
+        width=0,
+        height=0
+    ):
+        self.x=x
+        self.y=y
+        self.width=width
+        self.height=height
+
+    def draw(self,display):
+        pass
+
+    def on_event(self,event):
+        pass
