8154acc4003e9c232f5f6b4f8d789e79f75d51a9
Kenny <121385153+Kenny2022enroll@users.noreply.github.com>
2026-07-27 13:07:55 +0800

    更新 registry.py

--- apps/registry.py
+++ apps/registry.py
@@ -2,11 +2,7 @@ from apps.timer import Timer
 from apps.settings import Settings
 
 def load_apps():
-    apps = []
-    apps.append(
-        Timer()
-    )
-    apps.append(
+    return [
+        Timer(),
         Settings()
-    )
-    return apps
+    ]
