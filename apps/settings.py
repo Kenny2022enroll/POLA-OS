93466b9a16fd49237e6c1e189645a79aa7586f24
Kenny <121385153+Kenny2022enroll@users.noreply.github.com>
2026-08-16 14:43:30 +0800

    更新 settings.py

--- apps/settings.py
+++ apps/settings.py
@@ -1,10 +1,13 @@
 from core.app import App
+from ui.theme import Theme
+from core.event import BUTTON_B
 
 class Settings(App):
-    name="Settings"
-    def draw(self,display):
-        display.text(
-            "Settings",
-            30,
-            30
-        )
+    name = "Settings"
+    def draw(self, display):
+        display.text("Settings", 30, Theme.TITLE_Y)
+        display.text("v0.1 (Takla)", 30, Theme.CONTENT_Y)
+
+    def on_event(self, event):
+        if event.type == BUTTON_B:
+            return "BACK"
