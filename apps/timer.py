3ba2bf0639602233b5d042d9708000e91ae39baa
Kenny <121385153+Kenny2022enroll@users.noreply.github.com>
2026-08-16 14:43:09 +0800

    更新 timer.py

--- apps/timer.py
+++ apps/timer.py
@@ -1,41 +1,27 @@
 from core.app import App
 from ui.window import Window
 from ui.label import Label
+from ui.theme import Theme
+from core.event import BUTTON_B
 import time
 
 class Timer(App):
-    name="Timer"
+    name = "Timer"
     def open(self):
-        self.window=Window()
-        self.title=Label(
-            "Timer",
-            45,
-            5
-        )
-        self.number=Label(
-            "0",
-            60,
-            30
-        )
-        self.window.add(
-            self.title
-        )
-        self.window.add(
-            self.number
-        )
-        self.start=time.time()
+        self.window = Window()
+        self.title = Label("Timer", 45, Theme.TITLE_Y)
+        self.number = Label("0", 60, Theme.CONTENT_Y + 10)
+        self.window.add(self.title)
+        self.window.add(self.number)
+        self.start = time.time()
 
     def update(self):
-        value=str(
-            int(
-                time.time()
-                -
-                self.start
-            )
-        )
-        self.number.text=value
+        value = str(int(time.time() - self.start))
+        self.number.text = value
 
-    def draw(self,display):
-        self.window.draw(
-            display
-        )
+    def on_event(self, event):
+        if event.type == BUTTON_B:
+            return "BACK"
+
+    def draw(self, display):
+        self.window.draw(display)
