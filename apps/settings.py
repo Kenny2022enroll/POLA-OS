5d01ab3b30725116798bb8b1b091ebb65153ecd1
Kenny <121385153+Kenny2022enroll@users.noreply.github.com>
2026-08-18 12:53:06 +0800

    更新 settings.py

--- apps/settings.py
+++ apps/settings.py
@@ -1,13 +1,22 @@
 from core.app import App
 from ui.theme import Theme
-from core.event import BUTTON_B
+from ui.window import Window
+from ui.label import Label
+from core.event import NAV_NEXT, BACK
 
 class Settings(App):
     name = "Settings"
+    def open(self):
+        self.window = Window()
+        self.window.add(Label("Settings", 30, Theme.TITLE_Y))
+        self.window.add(Label("v0.1 (Takla)", 30, Theme.CONTENT_Y))
+
+    def update(self):
+        self.window.update()
+
     def draw(self, display):
-        display.text("Settings", 30, Theme.TITLE_Y)
-        display.text("v0.1 (Takla)", 30, Theme.CONTENT_Y)
+        self.window.draw(display)
 
     def on_event(self, event):
-        if event.type == BUTTON_B:
-            return "BACK"
+        if event.type == NAV_NEXT:
+            return BACK
