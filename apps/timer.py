74d19ad10e847af483fdd2d33e1a33b8fe865871
Kenny <121385153+Kenny2022enroll@users.noreply.github.com>
2026-08-18 12:52:23 +0800

    更新 timer.py

--- apps/timer.py
+++ apps/timer.py
@@ -2,7 +2,7 @@ from core.app import App
 from ui.window import Window
 from ui.label import Label
 from ui.theme import Theme
-from core.event import BUTTON_B
+from core.event import NAV_NEXT, BACK
 import time
 
 class Timer(App):
@@ -18,10 +18,11 @@ class Timer(App):
     def update(self):
         value = str(int(time.time() - self.start))
         self.number.text = value
+        self.window.update()
 
     def on_event(self, event):
-        if event.type == BUTTON_B:
-            return "BACK"
+        if event.type == NAV_NEXT:
+            return BACK
 
     def draw(self, display):
         self.window.draw(display)
