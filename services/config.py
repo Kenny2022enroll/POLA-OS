da7ba1adc4aab0e9b1a54b336062313a40fe2fe5
Kenny <121385153+Kenny2022enroll@users.noreply.github.com>
2026-08-19 10:14:19 +0800

    创建 config.py

---
+++ services/config.py
@@ -0,0 +1,62 @@
+try:
+    import ujson as json
+except ImportError:
+    import json
+import os
+
+DEFAULTS = {
+    "home_style": "default",
+    "brightness": 80,
+    "sleep_timeout": 60,
+    "sound_enabled": True,
+}
+
+class ConfigService:
+    def __init__(self, path="data/config.json"):
+        self.path = path
+        self.values = dict(DEFAULTS)
+        self.load()
+
+    def _ensure_parent(self):
+        parent = self.path.rsplit("/", 1)[0] if "/" in self.path else ""
+        if parent:
+            try:
+                os.stat(parent)
+            except OSError:
+                try:
+                    os.mkdir(parent)
+                except OSError:
+                    pass
+
+    def load(self):
+        try:
+            with open(self.path, "r") as stream:
+                loaded = json.load(stream)
+            for key in DEFAULTS:
+                if key in loaded:
+                    self.values[key] = loaded[key]
+        except Exception:
+            self.save()
+
+    def get(self, key, default=None):
+        return self.values[key] if key in self.values else default
+
+    def set(self, key, value, save=True):
+        self.values[key] = value
+        if save:
+            return self.save()
+        return True
+
+    def update(self, values):
+        for key, value in values.items():
+            self.values[key] = value
+        return self.save()
+
+    def save(self):
+        try:
+            self._ensure_parent()
+            with open(self.path, "w") as stream:
+                json.dump(self.values, stream)
+            return True
+        except Exception:
+            return False
