eefd978c9b9f7b55808707f18332bf4f4ecd151a
Kenny <121385153+Kenny2022enroll@users.noreply.github.com>
2026-08-19 10:28:38 +0800

    更新 label.py

--- ui/label.py
+++ ui/label.py
@@ -1,21 +1,9 @@
 from ui.widget import Widget
 
 class Label(Widget):
-    def __init__(
-        self,
-        text,
-        x,
-        y
-    ):
-        super().__init__(
-            x,
-            y
-        )
-        self.text=text
+    def __init__(self, text, x, y):
+        super().__init__(x, y)
+        self.text = text
 
-    def draw(self,display):
-        display.text(
-            self.text,
-            self.x,
-            self.y
-        )
+    def draw(self, display):
+        display.text(self.text, self.x, self.y)
